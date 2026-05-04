c4b14d4c94c7fd9fa779245d594f6eeb0babdaf8
Ton Nom <hugo.moise@etu.univ-tours.fr>
2026-05-04 22:05:39 +0200

    resolution du chemin le plus court

--- src/taxi/resolution.py
+++ src/taxi/resolution.py
@@ -1,8 +1,8 @@
-"""Fonctions de visualisation du graphe de taxi."""
+"""resolution du problème de trajet de taxi."""
 
 import networkx as nx
 
-from taxi.data import ProblemeTaxi
+from taxi.data import ProblemeTaxi, SolutionTaxi
 
 
 def construit_graphe(probleme: ProblemeTaxi) -> nx.Graph:
@@ -10,3 +10,27 @@ def construit_graphe(probleme: ProblemeTaxi) -> nx.Graph:
     for route in probleme.routes:
         resultat.add_edge(route.depart, route.arrivee, duree=route.duree)
     return resultat
+
+
+def resoud(probleme: ProblemeTaxi, depart: int, arrivee: int) -> SolutionTaxi | None:
+    graphe = construit_graphe(probleme=probleme)
+    try:
+        chemin = nx.shortest_path(
+            G=graphe,
+            source=depart,
+            target=arrivee,
+            weight="duree",
+        )
+        duree_totale = nx.shortest_path_length(
+            G=graphe,
+            source=depart,
+            target=arrivee,
+            weight="duree",
+        )
+    except nx.NetworkXException:
+        return None
+
+    return SolutionTaxi(
+        chemin=chemin,
+        duree_totale=duree_totale,
+    )
--- tests/test_resolution.py
+++ tests/test_resolution.py
@@ -1,7 +1,7 @@
 import networkx as nx
 
 from taxi.data import ProblemeTaxi, Route
-from taxi.resolution import construit_graphe
+from taxi.resolution import construit_graphe, resoud
 
 
 def test_construction_simple():
@@ -15,3 +15,29 @@ def test_construction_simple():
     attendu.add_edge(1, 2, duree=5)
 
     assert nx.utils.graphs_equal(calcule, attendu)
+
+
+def test_resolution_simple():
+    probleme = ProblemeTaxi(
+        emplacements=[1, 2, 3],
+        routes=[
+            Route(depart=1, arrivee=2, duree=5),
+            Route(depart=2, arrivee=3, duree=2),
+            Route(depart=1, arrivee=3, duree=10),
+        ],
+    )
+    resultat = resoud(probleme=probleme, depart=1, arrivee=3)
+
+    assert resultat is not None
+    assert resultat.chemin == [1, 2, 3]
+    assert resultat.duree_totale == 7
+
+
+def test_resolution_impossible():
+    probleme = ProblemeTaxi(
+        emplacements=[1, 2, 3],
+        routes=[Route(depart=1, arrivee=2, duree=5)],
+    )
+    resultat = resoud(probleme=probleme, depart=1, arrivee=3)
+
+    assert resultat is None
